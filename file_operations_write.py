e5bb7d889adfb9d577ad3e76467d01a966bb623b
jaimemtnz56 <jaimemtnzpz@icloud.com>
2025-02-04 17:06:54 +0100

    Commit of the operations on git

--- file_operations_write.py
+++ file_operations_write.py
@@ -1,4 +1,4 @@
-with open("story.txt", "a") as f: #"a" would be used to save the changes, abandoning the file
+with open("story.txt", "a") as f: #"a" would be used to save the changes, abandoning the file, append
     while True:
         text= input("What do you want to write {press q to exit}")
         if text == "q":
